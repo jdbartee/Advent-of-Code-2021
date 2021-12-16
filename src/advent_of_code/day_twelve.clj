70dea9490c72e570b6023f0efaebb0062227a965
JD Bartee <jd@bartee.net>
2021-12-15 18:03:44 -0600

    Playing with unit testing

--- src/advent_of_code/day_twelve.clj
+++ src/advent_of_code/day_twelve.clj
@@ -2,19 +2,39 @@
   (:require
    [clojure.java.io :as io]
    [clojure.set :as set]
-   [clojure.string :as string]))
+   [clojure.string :as string])
+   (:use [clojure.test]))
 
 (defn file-lines [fname]
   (with-open [rdr (io/reader fname)]
     (doall (line-seq rdr))))
 
-(defn parse-line [line]
-  (let [parts (string/split line #"-" 2)]
-        {(first parts) #{(last parts)} 
-         (last parts) #{(first parts)}}))
+(with-test
+  (defn parse-line [line]
+    (let [parts (string/split line #"-" 2)]
+          {(first parts) #{(last parts)} 
+           (last parts) #{(first parts)}}))
 
-(defn merge-graphs [a b]
-  (merge-with set/union a b))
+  (testing "PARSE-LINE"
+    (is (= 
+      (parse-line "a-b") 
+      {"a" #{"b"} "b" #{"a"}}))))
+
+(with-test
+  (defn merge-graphs [a b]
+    (merge-with set/union a b))
+
+  (testing "MERGE-GRAPHS"
+    (is 
+      (let [a-graph {"a" #{}}
+            b-graph {"b" #{}}
+            merged  {"a" #{} "b" #{}}]
+        (= (merge-graphs a-graph b-graph) merged)))
+    (is
+      (let [b-graph {"a" #{"b"}}
+            c-graph {"a" #{"c"}}
+            merged  {"a" #{"b c"}}] 
+        (= (merge-graphs b-graph c-graph) merged)))))
 
 (defn navigate-graph [g c v r x]
   (let [route (conj r c)
@@ -58,4 +78,5 @@
   (println (task-b "data/day12.input.txt")))
 
 (comment
+  (run-tests)
   (run))
